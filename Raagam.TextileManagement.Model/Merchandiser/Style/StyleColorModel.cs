c4605bdd831c8d9e67ac0cd616e270d0289a20ac
SpiderISoft <ganeshan@spiderisoft.com>
2014-01-01 15:58:24 +0530

    Style Creation

--- Raagam.TextileManagement.Model/Merchandiser/Style/StyleColorModel.cs
+++ Raagam.TextileManagement.Model/Merchandiser/Style/StyleColorModel.cs
@@ -5,8 +5,10 @@ using System.Text;
 
 namespace Raagam.TextileManagement.Model 
 {
+
     public class StyleColorModel
     {
+
         public long StyleColorSequenceNumber { get; set; }
         public long StyleSequenceNumber { get; set; }
         public long ColorSequenceNumber { get; set; }
---
+++ Raagam.TextileManagement.Model/Merchandiser/Style/StyleModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Raagam.TextileManagement.Model 
+{
+    [DataContract]
+    public class StyleModel
+    {
+        [DataMember]
+        public long StyleSequenceNumber { get; set; }
+
+        [DataMember]
+        public string StyleName { get; set; }
+
+        [DataMember]
+        public string StyleDescription { get; set; }
+
+        [DataMember]
+        public long StyleTypeSequenceNumber { get; set; }
+
+        [DataMember]
+        public byte[] StyleImage { get; set; }
+
+        [DataMember]
+        public bool IsCompleted { get; set; }
+
+        [DataMember]
+        public Raagam.TextileManagement.CommonUtility.EnumConstants.ModelCurrentState State { get; set; }
+ 
+    }
+}
+ 
